229b9f146d9abb34a6a8e2e63fa81aca8bd0ec8d
alexis <agschube@gmail.com>
2013-08-09 23:54:48 -0700

    added logging

--- playgame.py
+++ playgame.py
@@ -4,23 +4,22 @@ import gtr
 from gamestate import GameState
 from player import Player
 import pprint
+import logging
 
 pp = pprint.PrettyPrinter(indent=4)
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
 
 players = []
 for i in range(1,4):
   player = Player('Player {0:d}'.format(i))
   players.append(player)
 
-print 'Starting a game...'
 game = gtr.Game(GameState(players))
 #print repr(game)
 
-print 'Initializing the game...'
-game.init_common_piles(len(players))
+game.init_common_piles(n_players=len(players))
 #print repr(game)
 
-print 'Initializing the players...'
 game.game_state.init_players()
 #pp.pprint(vars(game))
 
