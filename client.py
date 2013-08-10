50d36ed1a223fc3a50691f925cf6cbd1b6151cdf
alexis <agschube@gmail.com>
2013-08-10 14:37:41 -0700

    added ability to wait for turn

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-A simple client.
+A simple client.  Maybe this should be a class
 10 Aug 2013 Starting with pickle -- AGS
 """
 
@@ -25,6 +25,7 @@ def get_game_state(log_file_name='log_state.log'):
   """
   log_file = file(log_file_name, 'r')
   game_state = pickle.load(log_file)
+  log_file.close()
   return game_state
 
 def save_game_state(game_state, log_file_name='log_state.log'):
@@ -35,7 +36,25 @@ def save_game_state(game_state, log_file_name='log_state.log'):
   pickle.dump(game_state, log_file)
   log_file.close()
 
-
+def wait_for_my_turn(my_index):
+  """
+  Wait until this player has priority
+  """
+  logging.info('--> Waiting for your turn...')
+  previous_priority_index = None
+  priority_index = None
+  # keep checking game state until it is my turn
+  while my_index is not priority_index:
+    game_state = get_game_state()
+    priority_index = game_state.priority_index
+    leader_index = game_state.leader_index
+    # print info about changes to priority
+    if previous_priority_index is not priority_index:
+      previous_priority_index = priority_index
+      # print the current game state after every change
+      game = gtr.Game(game_state=get_game_state())
+      game.show_public_game_state()
+      
 
 def main():
 
@@ -44,39 +63,44 @@ def main():
 
   logging.info('--> Welcome to the game!')
 
+  # add player to game
+  my_name = raw_input('Enter your name: ')
+
+  # check whether game exists, if not start a new one
   try:
     game_state = get_game_state()
     logging.info('--> Joining existing game...')  
-
   except IOError:
     logging.info('--> Starting a new game...')
     game_state = GameState()
 
-  name = raw_input('Enter your name: ')
-  player = Player(name=name)
-  game_state.add_player(player)
+  my_index = game_state.add_player(Player(name=my_name))
   save_game_state(game_state)
 
+
+  # prompt player to join game when desired number of players is reached
   response = 'n'
-  while response is 'n':
+  while response is not 'y':
     game_state = get_game_state()
     n_players = game_state.get_n_players() 
     print '--> There are {n!r} players.'.format(n=n_players)
     response = raw_input('--> Would you like to start? [y/n]: ')
-    if response is 'q':
-      return
+    if response is 'y':
+      game_state = get_game_state()
 
-  game = gtr.Game(game_state=game_state)
-  game.init_common_piles(n_players=n_players)
-  game.game_state.init_players()
+      # check whether someone else has started the game
+      if not game_state.is_started:
+        game = gtr.Game(game_state=game_state)
+        game_state.is_started = True
+        # initialize the game
+        game.init_common_piles(n_players=n_players)
+        game.game_state.init_players()
+        save_game_state(game_state=game_state)
 
-  test_state = game.game_state
-  print test_state
-  game.show_public_game_state()
-
-  save_game_state(game_state)
+  wait_for_my_turn(my_index=my_index)
 
 
 if __name__ == '__main__':
-    main()
+
+  main()
 
