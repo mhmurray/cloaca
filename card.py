8195b9ea0e96b6c86e4c7effeaaf2c74441626bb
alexis <agschube@gmail.com>
2013-08-10 00:25:00 -0700

    improved output

--- card.py
+++ card.py
@@ -5,19 +5,21 @@ A class to encapsulate a card.
 """
 
 class Card:
-  def __init__(self, name=None, material=None, value=None, role=None):
+  def __init__(self, name=None, material=None, value=None, role=None,
+    description=None):
     self.name = name or ''
     self.short_name = name[:4] if name else ''
     self.material = material
     self.value = value or 0
     self.role = role
     self.is_jack = False
+    self.description = description
 
   def __repr__(self):
-    rep=('Card(name={name!r}, material={material!r}, '
-         'value={value!r}, role={role!r})')
+    rep=('Card: {name!r} | {material!r} '
+         '| {value!r} | {role!r} | {description!r}')
     return rep.format(name=self.name, material=self.material,
-      value=self.value, role=self.role)
+      value=self.value, role=self.role, description=self.description)
 
   def __str__(self):
     return self.name
@@ -25,7 +27,13 @@ class Card:
 
 if __name__ == '__main__':
 
-    test = Card()
+    test = Card(
+        name='Test', 
+        material='Wood',
+        value='?',
+        role='Craftsman',
+        description='Test card',    
+    )
     print test
     print repr(test)
 
