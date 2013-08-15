a6e87690e6bc9a22ec21874ef2c821f3a2855212
Michael Murray <mhmurray@uw.edu>
2013-08-15 00:16:51 -0700

    Remove self argument from _get_card_from_zone

--- gtrutils.py
+++ gtrutils.py
@@ -1,7 +1,7 @@
 """ Utility functions for GTR.
 """
 
-def _get_card_from_zone(self, card, l):
+def _get_card_from_zone(card, l):
   """ Wrapper around the possible exception caused by trying to 
   find a non-existent card in a list. Prints an error and 
   re-raises the exception.
