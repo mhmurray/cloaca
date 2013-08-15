37cd8b3953393a94d7c13d52f97096733a3b6ab4
Michael Murray <mhmurray@uw.edu>
2013-08-15 14:54:54 -0700

    Resolve merge

--- gtrutils.py
+++ gtrutils.py
@@ -1,14 +1,29 @@
+#!/usr/bin/env python
+
+import logging
+
 """ Utility functions for GTR.
 """
 
-def _get_card_from_zone(card, l):
+def get_card_from_zone(card, zone):
   """ Wrapper around the possible exception caused by trying to 
   find a non-existent card in a list. Prints an error and 
   re-raises the exception.
   """
+  logging.debug('getting card {0!s} from zone {1!s}'.format(card, zone))
   try:
-    return l.pop(l.index(card))
+    return zone.pop(zone.index(card))
   except ValueError as e:
-    print 'Error! card {0!s} not found'.format(card)
+    logging.error('Error! card {0!s} not found'.format(card))
     raise
+
+
+if __name__ == '__main__':
+
+  # simple tests...
+  logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+  
+  test_zone = ['a', 'a', 'b']
+  get_card_from_zone('a', test_zone)
+  get_card_from_zone('c', test_zone)
 
