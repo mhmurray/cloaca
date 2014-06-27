4df6a2533773eba0b1d72178ba4d1e90265a3945
Michael Murray <mhmurray@uw.edu>
2014-06-26 22:11:22 -0700

    Incremental update

--- mvcserver.py
+++ mvcserver.py
@@ -131,166 +131,6 @@ manipulating the game state to take that action.
 # ACTION(resolve_<buildings>) : etc. for all buildings
 # ACTION(kids_in_pool) : senate, sewer hooks into this
 
-def take_turn(self, player):
-  """
-  1) Ask for thinker or lead
-  2) -->ACTION(thinker), -->ACTION(lead_role)
-  """
-  pass
-
-def perform_thinker_action(self, thinking_player):
-  """
-  1) If thinking_player has a Latrine, ask for discard.
-  2) If thinking_player has a Vomitorium, ask for discard.
-  3) Determine # cards that would be drawn. Check hand size,
-     Temple, and Shrine. Also check if jacks are empty,
-     and if drawing cards would end the game.
-  3) Ask thinking_player for thinker type (jack or # cards)
-  4) Draw cards for player.
-  """
-  pass
-
-def lead_role_action(self, leading_player):
-  """
-  1) Ask for cards used to lead
-  2) Check legal leads using these cards (Palace, petition)
-  3) Ask for role clarification if necessary
-  4) Move cards to camp, set this turn's role that was led.
-  """
-  pass
-
-def follow_role_action(self, following_player, role):
-  """
-  1) Ask for cards used to follow
-  2) Check for ambiguity in ways to follow (Crane, Palace, petition)
-  3) Ask for clarification if necessary
-  4) Move cards to camp
-  """
-  pass
-
-def perform_lead_role(self, leading_player):
-  """
-  This is the main part of a player's turn. It figures how many actions
-  the player gets from leading or following with a card (or cards).
-  It then figures out how many clients get to perform their actions and
-  activates them in turn. If the number of clients changes, it must be
-  tracked here (eg. the first client finishes a Ludus Magna).
-  1) Determine if the player gets to perform multiple actions (Palace)
-  2) If the role is architect or craftsman, if the player is going
-     to get multiples (Palace, clietele) or the have a Tower, set
-     out_of_town_allowed=true
-  3) While the player has actions {-->ACTION(perform_<role>_action)}
-  4) Check how many clientele the player has (Stockpile, Ludus Magna).
-  5) While the player has clientele actions { -->ACTION(perform_clientele_action) }
-     Have to forward out_of_town_allowed.
-  """
-  pass
-
-def perform_clientele_action(self, player, role, out_of_town_allowed):
-  """
-  This function will activate one client. It makes two actions 
-  if the player has a Circus Maximus. This function doesn't keep track
-  of which clients have been used.
-  1) -->ACTION(perform_<role>_action), forwarding out_of_town_allowed to architect/craftsman
-  1) If the player has a Circus Maximus, do it again
-  """
-  pass
-
-
-def perform_laborer_action(self, player):
-  """
-  1) Ask for which card from the pool
-  2) Move card from pool
-  3) Check for Dock and ask for card from hand
-  4) Move card from hand
-  """
-  pass
-
-def peform_patron_action(self, player):
-  """
-  1) Abort if clientele full (Insula, Aqueduct)
-  2) Ask for which card from pool
-  3) Check for Bar and Aqueduct and 
-  """
-  pass
-
-def peform_craftsman_action(self, player, out_of_town_allowed):
-  """
-  out_of_town_allowed is indicated by the caller if this craftsman would
-  be stacked up with another, so that an out-of-town site may be used.
-  In that case, this will return an indication and the caller can nix the
-  next craftsman action.
-  1) Ask for building to start or material to add.
-  2) If out_of_town_allowed is false, don't allow out of town, otherwise
-     start the out-of-town site and return the indicator.
-  3) Check legality of material, building + site.
-  4) Place material or building -->ACTION(place_material) -->ACTION(start_building)
-  5) Mark flag for "performed craftsman this turn" for Academy
-  """
-  pass
-
-def peform_legionary_action(self, player, affected_players):
-  """
-  affected_players must be determined by the caller, accounting for Pallisade, Wall, Bridge
-  1) Ask for card to show for demand
-  2) Ask for affected players to give card of material, or say "Glory to Rome!"
-  3) If player has coliseum, ask for affected players to select client to send to the lions.
-  4) If player has bridge, ask affected players for material from stockpile
-  """
-  pass
-
-def peform_architect_action(self, player, out_of_town_allowed):
-  """
-  out_of_town_allowed is indicated by the caller if this architect would
-  be stacked up with another, so that an out-of-town site may be used.
-  In that case, this will return an indication and the caller can nix the
-  next architect action.
-  1) Ask for building to start or material to add. (Archway, Stairway)
-  2) If out_of_town_allowed is false, don't allow out of town, otherwise
-     start the out-of-town site and return the indicator.
-  3) Check legality of material, building + site.
-  4) Place material or building -->ACTION(place_material) -->ACTION(start_building)
-  """
-  pass
-
-def peform_merchant_action(self, player):
-  """
-  Do we log materials? We should in case the display messes up,
-  but maybe only until end of turn.
-  1) Abort if vault full. Also between each step here. (Market)
-  2) Ask player to select material from Stockpile. Reveal and place in vault.
-  3) If Basilica, ask player to select from hand. No reveal and vault.
-  4) If Atrium, ask player to select top of deck. No reveal and vault.
-  """
-  pass
-
-def kids_in_pool(self, player, players_with_senate):
-  """
-  Place cards in camp into the pool.
-  1) If Sewer, ask to move cards into stockpile.
-  2) If dropping a Jack, ask players_with_senate in order.
-  """
-  pass
-
-def end_turn(self, player):
-  """
-  Ask for Academy thinker. Need to figure out whether or not Senate goes first.
-  """
-  pass
-
-def add_material_to_building(self, player, material, source, building):
-  """
-  Adds a material to a building and checks if the building is complete
-  The caller should make sure it's legal.
-  1) Add material to building, indicate Stairway separately.
-  2) If building is completed, trigger resolve_<building> action
-  """
-  pass
-
-def resolve_building(self, player, building):
-  """ Placeholder for all building resolutions
-  """
-  pass
 
 class MVCServer:
   """ Manipulates the game state at the requests of registered clients.
@@ -306,8 +146,170 @@ class MVCServer:
   def run(self):
     """ Main game loop """
     player = initialize_and_get_starting_player()
-    take_turn(leader)
+    take_turn(player)
 
 
+  def take_turn(self, player):
+    """
+    1) Ask for thinker or lead
+    2) -->ACTION(thinker), -->ACTION(lead_role)
+    """
+    pass
+
+  def perform_thinker_action(self, thinking_player):
+    """
+    1) If thinking_player has a Latrine, ask for discard.
+    2) If thinking_player has a Vomitorium, ask for discard.
+    3) Determine # cards that would be drawn. Check hand size,
+       Temple, and Shrine. Also check if jacks are empty,
+       and if drawing cards would end the game.
+    3) Ask thinking_player for thinker type (jack or # cards)
+    4) Draw cards for player.
+    """
+    pass
+
+  def lead_role_action(self, leading_player):
+    """
+    1) Ask for cards used to lead
+    2) Check legal leads using these cards (Palace, petition)
+    3) Ask for role clarification if necessary
+    4) Move cards to camp, set this turn's role that was led.
+    """
+    pass
+
+  def follow_role_action(self, following_player, role):
+    """
+    1) Ask for cards used to follow
+    2) Check for ambiguity in ways to follow (Crane, Palace, petition)
+    3) Ask for clarification if necessary
+    4) Move cards to camp
+    """
+    pass
+
+  def perform_role_being_led(self, player):
+    """
+    This is the main part of a player's turn. It figures how many actions
+    the player gets from leading or following with a card (or cards).
+    It then figures out how many clients get to perform their actions and
+    activates them in turn. If the number of clients changes, it must be
+    tracked here (eg. the first client finishes a Ludus Magna).
+    1) Determine if the player gets to perform multiple actions (Palace)
+    2) If the role is architect or craftsman, if the player is going
+       to get multiples (Palace, clietele) or the have a Tower, set
+       out_of_town_allowed=true
+    3) While the player has actions {-->ACTION(perform_<role>_action)}
+    4) Check how many clientele the player has (Stockpile, Ludus Magna).
+    5) While the player has clientele actions { -->ACTION(perform_clientele_action) }
+       Have to forward out_of_town_allowed.
+    """
+    pass
+
+  def perform_clientele_action(self, player, role, out_of_town_allowed):
+    """
+    This function will activate one client. It makes two actions 
+    if the player has a Circus Maximus. This function doesn't keep track
+    of which clients have been used.
+    1) -->ACTION(perform_<role>_action), forwarding out_of_town_allowed to architect/craftsman
+    1) If the player has a Circus Maximus, do it again
+    """
+    pass
+
+
+  def perform_laborer_action(self, player):
+    """
+    1) Ask for which card from the pool
+    2) Move card from pool
+    3) Check for Dock and ask for card from hand
+    4) Move card from hand
+    """
+    pass
+
+  def peform_patron_action(self, player):
+    """
+    1) Abort if clientele full (Insula, Aqueduct)
+    2) Ask for which card from pool
+    3) Check for Bar and Aqueduct and 
+    """
+    pass
+
+  def peform_craftsman_action(self, player, out_of_town_allowed):
+    """
+    out_of_town_allowed is indicated by the caller if this craftsman would
+    be stacked up with another, so that an out-of-town site may be used.
+    In that case, this will return an indication and the caller can nix the
+    next craftsman action.
+    1) Ask for building to start or material to add.
+    2) If out_of_town_allowed is false, don't allow out of town, otherwise
+       start the out-of-town site and return the indicator.
+    3) Check legality of material, building + site.
+    4) Place material or building -->ACTION(place_material) -->ACTION(start_building)
+    5) Mark flag for "performed craftsman this turn" for Academy
+    """
+    pass
+
+  def peform_legionary_action(self, player, affected_players):
+    """
+    affected_players must be determined by the caller, accounting for Pallisade, Wall, Bridge
+    1) Ask for card to show for demand
+    2) Ask for affected players to give card of material, or say "Glory to Rome!"
+    3) If player has coliseum, ask for affected players to select client to send to the lions.
+    4) If player has bridge, ask affected players for material from stockpile
+    """
+    pass
+
+  def peform_architect_action(self, player, out_of_town_allowed):
+    """
+    out_of_town_allowed is indicated by the caller if this architect would
+    be stacked up with another, so that an out-of-town site may be used.
+    In that case, this will return an indication and the caller can nix the
+    next architect action.
+    1) Ask for building to start or material to add. (Archway, Stairway)
+    2) If out_of_town_allowed is false, don't allow out of town, otherwise
+       start the out-of-town site and return the indicator.
+    3) Check legality of material, building + site.
+    4) Place material or building -->ACTION(place_material) -->ACTION(start_building)
+    """
+    pass
+
+  def peform_merchant_action(self, player):
+    """
+    Do we log materials? We should in case the display messes up,
+    but maybe only until end of turn.
+    1) Abort if vault full. Also between each step here. (Market)
+    2) Ask player to select material from Stockpile. Reveal and place in vault.
+    3) If Basilica, ask player to select from hand. No reveal and vault.
+    4) If Atrium, ask player to select top of deck. No reveal and vault.
+    """
+    pass
+
+  def kids_in_pool(self, player, players_with_senate):
+    """
+    Place cards in camp into the pool.
+    1) If Sewer, ask to move cards into stockpile.
+    2) If dropping a Jack, ask players_with_senate in order.
+    """
+    pass
+
+  def end_turn(self, player):
+    """
+    Ask for Academy thinker. Need to figure out whether or not Senate goes first.
+    1) Find players_with_senate
+    2) --> kids_in_pool(player, players_with_senate)
+    """
+    pass
+
+  def add_material_to_building(self, player, material, source, building):
+    """
+    Adds a material to a building and checks if the building is complete
+    The caller should make sure it's legal.
+    1) Add material to building, indicate Stairway separately.
+    2) If building is completed, trigger resolve_<building> action
+    """
+    pass
+
+  def resolve_building(self, player, building):
+    """ Placeholder for all building resolutions
+    """
+    pass
 
 
