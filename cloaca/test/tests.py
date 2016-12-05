1b83c530a4ddc5c96551481cf69515ef37fd7e0e
Michael Murray <mhmurray@uw.edu>
2016-12-04 17:17:15 -0800

    New command line options, especially for verbosity

--- cloaca/test/tests.py
+++ cloaca/test/tests.py
@@ -30,21 +30,33 @@ def setup_logging(
 
 
 def main():
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser = argparse.ArgumentParser(
+            description=DESCRIPTION,
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--all', action='store_true',
-            help='Run all tests.')
+            help='Run all tests instead of matching pattern.')
     parser.add_argument('pattern', nargs='*',
             help=('pattern(s) to match against, eg. "buildings" or '
                 '"architect.TestArchitect.test_lead_architect".'))
-    parser.add_argument('--verbose', action='store_true',
-            help='Enable INFO/DEBUG logging output.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+            help='Use verbose test result reporting.')
+    parser.add_argument('-q', '--quiet', action='store_true',
+            help=('Suppress individual test result reporting. Still reports '
+                  'summary information. Overrides --verbose.'))
+    parser.add_argument('--log-level', default='WARNING',
+            help=('Set app log level during tests. Valid arguments are: '
+                  'DEBUG, INFO, WARNING, ERROR, CRITICAL. See logging module '
+                  'documentation.'))
     args = parser.parse_args()
 
     setup_logging()
 
-    if args.verbose:
-        # This catches the children loggers like cloaca.game
-        logging.getLogger('cloaca').setLevel(logging.DEBUG)
+    numeric_level = getattr(logging, args.log_level.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: {0!s}'.format(args.log_level))
+
+    # This catches the children loggers like cloaca.game
+    logging.getLogger('cloaca').setLevel(numeric_level)
 
     loader = unittest.defaultTestLoader
 
@@ -63,7 +75,17 @@ def main():
         suites = loader.loadTestsFromNames(args.pattern)
 
     test_suite = unittest.TestSuite(suites)
-    test_runner = unittest.TextTestRunner().run(test_suite)
+    
+    # TextTestRunner takes verbosity that can be 0 (quiet), 1 (default),
+    # or 2 (verbose). Quiet overrides verbose.
+    if args.quiet:
+        verbosity = 0
+    elif args.verbose:
+        verbosity = 2
+    else:
+        verbosity=1
+
+    test_runner = unittest.TextTestRunner(verbosity=verbosity).run(test_suite)
 
 if __name__ == '__main__':
     main()
